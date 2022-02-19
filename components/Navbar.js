b5d94178e8aa37c3ec10595af6bc29fd350caf61
Samuel Adjei <info.samadjei@gmail.com>
2022-02-19 18:48:21 +0000

    added fixed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import { FiMenu } from 'react-icons/fi';
 import { FiX } from 'react-icons/fi';
-import { FiTriangle } from 'react-icons/fi';
-import ContactDropdown from './ContactDropdown';
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -11,35 +9,36 @@ const Navbar = () => {
 		setIsOpen(!isOpen);
 	};
 	return (
-		<div className={isOpen ? 'bg-section-bg text-headline ' : 'shadow bg-background'}>
-			<nav className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 py-7 w-full relative">
+		<nav className={isOpen ? 'bg-section-bg text-headline fixed w-full z-10' : 'shadow bg-background fixed z-10 w-full'}>
+			<div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 py-7 w-full relative">
 				<div>
 					<Link href="/">
 						<a className="text-xl">Sam Adjei</a>
 					</Link>
 				</div>
-				<ul className={isOpen ? 'absolute left-0 px-8 flex top-20 gap-8 flex-col w-full bg-section-bg text-headline z-30 pb-8 shadow' : 'hidden md:flex items-center gap-8 text-lg '}>
-					{/* <li className="cursor-pointer">
-						<a>About</a>
-					</li> */}
-					<li className="cursor-pointer">
-						{/* <Link href="/Projects"> */}
-						<a href="#projects">Work</a>
-						{/* </Link> */}
-					</li>
-					<li className="cursor-pointer">
-						<div className="flex items-center gap-2">
-							{/* <Link href="/"> */}
-							<a href="mailto:samueladj3i@gmail.com">Connect</a>
-							{/* </Link> */}
-							{/* <FiTriangle className="rotate-180 text-xs" /> */}
-						</div>
-						{/* <ContactDropdown /> */}
-					</li>
+				<ul className="gap-8 hidden md:flex items-center text-lg">
+					{['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+						<li key={`link-${{ item }}`}>
+							<a className="capitalize" href={`#${{ item }}`}>
+								{item}
+							</a>
+						</li>
+					))}
 				</ul>
+				<div className="relative md:hidden block">
+					<ul className={isOpen ? 'fixed flex left-0 right-0 px-8 top-20 gap-8 flex-col w-full bg-section-bg text-headline z-30 pb-8 shadow' : 'hidden'}>
+						{['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+							<li key={`link-${{ item }}`}>
+								<a className="capitalize" href={`#${{ item }}`}>
+									{item}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
 				{isOpen ? <FiX className="md:hidden cursor-pointer text-3xl" onClick={menuToggle} /> : <FiMenu className="md:hidden cursor-pointer text-3xl" onClick={menuToggle} />}
-			</nav>
-		</div>
+			</div>
+		</nav>
 	);
 };
 
